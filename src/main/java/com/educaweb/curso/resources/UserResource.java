5705f11481ddda34ef927256b57fe611954471ac
JoaoTorpe <victotorpe10@gmail.com>
2023-07-02 12:22:40 -0300

    User entity and resource

---
+++ src/main/java/com/educaweb/curso/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.educaweb.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educaweb.curso.entities.User;
+
+@RestController
+@RequestMapping (value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "joao","victo@", "9999","12345");
+		
+		return ResponseEntity.ok().body(u);
+		
+	}
+
+}
