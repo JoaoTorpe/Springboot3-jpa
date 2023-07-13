34e8b091608b7df204793ad4c773bbb1a55dd7ce
JoaoTorpe <victotorpe10@gmail.com>
2023-07-13 15:19:15 -0300

    Payment, one-to-one association

--- src/main/java/com/educaweb/curso/entities/Order.java
+++ src/main/java/com/educaweb/curso/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.educaweb.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_order")
@@ -36,6 +38,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.Order")
 	private Set<OrderItem> Orderitems= new HashSet<>();
+	
+	@OneToOne(mappedBy = "Order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
@@ -66,6 +71,15 @@ public class Order implements Serializable {
 
 	public long getId() {
 		return id;
+	}
+
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Set<OrderItem> getOrderitems() {
---
+++ src/main/java/com/educaweb/curso/entities/Payment.java
@@ -0,0 +1,68 @@
+package com.educaweb.curso.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "payment_tb")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	 private Instant moment;
+	 
+	 @OneToOne
+	 @MapsId
+	 private Order Order;
+	 
+	 public Payment () {
+		 
+	 }
+	public Payment(Instant moment, com.educaweb.curso.entities.Order order) {
+		super();
+		this.moment = moment;
+		Order = order;
+	}
+	public Instant getMoment() {
+		return moment;
+	}
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+	public Order getOrder() {
+		return Order;
+	}
+	public void setOrder(Order order) {
+		Order = order;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+	 
+	 
+	
+}
